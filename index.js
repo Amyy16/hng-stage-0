16bb8ab3d6757f6bc0e390aaec460c7f00258a24
Amarachi <nwabueze2amarachi@gmail.com>
2025-01-29 15:04:19 +0100

    inital commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors")
+require("dotenv").config();
+
+
+const app = express();
+app.use(express.json());
+app.use(cors())
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res)=>{
+  const data = {
+    email: "nwabueze2amarachi@gmail.com",
+    current_datetime: new Date().toISOString().split(".") [0] + "Z",
+    github_url: "<www.github.com>"
+  }
+  res.status(200).json(data)
+})
+
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`);
+});
